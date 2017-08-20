1fa0765efe8452754909f25408cbb6a6f9877f73
Keks <keks@htmlacademy.ru>
2017-08-20 21:44:24 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/map.js
+++ 2/js/map.js
@@ -3,31 +3,48 @@
 /**
  * Объявление
  * @typedef {object} Ad
- * @property {string} avatar - Аватар пользователя
- * @property {string} title - Заголовок
- * @property {string} address - Адрес
- * @property {number} price - Цена
- * @property {number} rooms - Количество комнат
- * @property {number} guests - Количество гостей
- * @property {string} checkin - Время заезда
- * @property {string} checkout - Время выезда
- * @property {Array.<string>} features - Особенности
- * @property {string} description - Описание
- * @property {array} photos - Фото
- * @property {number} x - Координата x метки на карте
- * @property {number} y - Координата y метки на карте
+ * @property {object} author - Автор объявления
+ * @property {string} author.avatar - Аватар автора
+ * @property {object} offer - Предложение
+ * @property {string} offer.title - Заголовок
+ * @property {string} offer.address - Адрес
+ * @property {number} offer.price - Цена
+ * @property {number} offer.rooms - Количество комнат
+ * @property {number} offer.guests - Количество гостей
+ * @property {string} offer.checkin - Время заезда
+ * @property {string} offer.checkout - Время выезда
+ * @property {Array.<string>} offer.features - Особенности
+ * @property {string} offer.description - Описание
+ * @property {array} offer.photos - Фото
+ * @property {object} location - Локация
+ * @property {number} location.x - Координата x метки на карте
+ * @property {number} location.y - Координата y метки на карте
  */
 
-var numberOfAds = 8;
+var NUMBER_OF_ADS = 8;
+var PIN_SIZE = {
+  WIDTH: 56,
+  HEIGHT: 75
+};
 var AD_TEMPLATE = {
-  MIN_ROOMS: 1,
-  MAX_ROOMS: 5,
-  MIN_PRICE: 100,
-  MAX_PRICE: 1000000,
-  X_MIN: 300,
-  X_MAX: 900,
-  Y_MIN: 100,
-  Y_MAX: 500,
+  ROOMS: {
+    MIN: 1,
+    MAX: 5
+  },
+  PRICES: {
+    MIN: 100,
+    MAX: 1000000
+  },
+  LOCATION: {
+    X: {
+      MIN: 300,
+      MAX: 900
+    },
+    Y: {
+      MIN: 100,
+      MAX: 500
+    }
+  },
   TITLE: ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'],
   TYPE: ['flat', 'house', 'bungalo'],
   FEATURES: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
@@ -35,84 +52,76 @@ var AD_TEMPLATE = {
 };
 
 /**
- * Возвращает рандомное число от min до max
  * @param {number} min
  * @param {number} max
- * @return {number}
+ * @return {number} Рандомное число от min до max
  */
 function getRandomNumber(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
 /**
- * Возвращает рандомный элемент массива
  * @param {array} array
- * @return {*}
+ * @return {*} Рандомный элемент массива
  */
 var getRandomArrayElem = function (array) {
-  return array[Math.round(Math.random() * (array.length - 1))];
+  return array[getRandomNumber(0, array.length - 1)];
 };
 
 /**
  * Создаёт массив, состоящий из сгенерированных объектов
  * @param {number} length
  * @param {function} getItem
- * @param {boolean} checkItem
+ * @param {boolean} uniq - Если true, элементы в массиве будут уникальными
  * @return {array}
  */
-var getRandomArray = function (length, getItem, checkItem) {
+var getRandomArray = function (length, getItem, uniq) {
   var items = [];
   while (items.length !== length) {
-    if (checkItem) {
-      var item = getItem();
+    var item = getItem();
+    if (uniq) {
       if (!items.includes(item)) {
         items.push(item);
       }
     } else {
-      items.push(getItem());
+      items.push(item);
     }
   }
   return items;
 };
 
 /**
- * Возвращает случайные координаты
- * @param {number} minX
- * @param {number} minY
- * @param {number} maxX
- * @param {number} maxY
- * @return {object}
+ * @param {object} location
+ * @return {object} Случайные координаты
  */
-var getLocation = function (minX, minY, maxX, maxY) {
+var getLocation = function (location) {
   return {
-    x: getRandomNumber(minX, maxX),
-    y: getRandomNumber(minY, maxY)
+    x: getRandomNumber(location.X.MIN, location.X.MAX),
+    y: getRandomNumber(location.Y.MIN, location.Y.MAX)
   };
 };
 
 /**
- * Создаёт массив номеров
- * @return {Array.<number>}
+ * @return {Array.<number>} Массив номеров
  */
 var getImageAddress = function () {
-  return getRandomArray(numberOfAds, function () {
-    return getRandomNumber(1, numberOfAds);
+  return getRandomArray(NUMBER_OF_ADS, function () {
+    var number = getRandomNumber(1, NUMBER_OF_ADS).toString();
+    return number.length === 1 ? '0' + number : number;
   }, true);
 };
 
 /**
- * Создаёт массив заголовков
- * @return {Array.<string>}
+ * @return {Array.<string>} Массив заголовков
  */
 var getTitles = function () {
-  return getRandomArray(numberOfAds, function () {
+  return getRandomArray(NUMBER_OF_ADS, function () {
     return getRandomArrayElem(AD_TEMPLATE.TITLE);
   }, true);
 };
 
 /**
- * Создаёт массив строк случайной длины
- * @return {Array.<string>}
+ * @return {Array.<string>} Массив строк случайной длины
  */
 var getFeatures = function () {
   return getRandomArray(getRandomNumber(1, AD_TEMPLATE.FEATURES.length - 1), function () {
@@ -120,18 +129,15 @@ var getFeatures = function () {
   }, true);
 };
 
-var imageAddresses = getImageAddress();
-var titles = getTitles();
-
 /**
  * Создаёт рандомное объявление
  * @return {Ad}
  */
 var createAd = function () {
-  var location = getLocation(AD_TEMPLATE.X_MIN, AD_TEMPLATE.Y_MIN, AD_TEMPLATE.X_MAX, AD_TEMPLATE.Y_MAX);
+  var location = getLocation(AD_TEMPLATE.LOCATION);
   return {
     author: {
-      avatar: 'img/avatars/user0' + imageAddresses.pop() + '.png',
+      avatar: 'img/avatars/user' + imageAddresses.pop() + '.png',
     },
     location: {
       x: location.x,
@@ -140,9 +146,9 @@ var createAd = function () {
     offer: {
       title: titles.pop(),
       address: location.x + ', ' + location.y,
-      price: getRandomNumber(AD_TEMPLATE.MIN_PRICE, AD_TEMPLATE.MAX_PRICE),
+      price: getRandomNumber(AD_TEMPLATE.PRICES.MIN, AD_TEMPLATE.PRICES.MAX),
       type: getRandomArrayElem(AD_TEMPLATE.TYPE),
-      rooms: getRandomNumber(AD_TEMPLATE.MIN_ROOMS, AD_TEMPLATE.MAX_ROOMS),
+      rooms: getRandomNumber(AD_TEMPLATE.ROOMS.MIN, AD_TEMPLATE.ROOMS.MAX),
       guests: getRandomNumber(1, 10),
       checkin: getRandomArrayElem(AD_TEMPLATE.TIME),
       checkout: getRandomArrayElem(AD_TEMPLATE.TIME),
@@ -161,8 +167,8 @@ var createAd = function () {
 var createPin = function (ad) {
   var div = document.createElement('div');
   div.classList.add('pin');
-  div.style.left = ad.location.x + 'px';
-  div.style.top = ad.location.y + 'px';
+  div.style.left = ad.location.x - PIN_SIZE.WIDTH / 2 + 'px';
+  div.style.top = ad.location.y - PIN_SIZE.HEIGHT + 'px';
   div.innerHTML = '<img src="' + ad.author.avatar + '" class="rounded" width="40" height="40">';
   return div;
 };
@@ -183,28 +189,18 @@ var getFragment = function (array, fn) {
   return fragment;
 };
 
-var ads = getRandomArray(numberOfAds, createAd, false);
-var pinsList = getFragment(ads, createPin);
-var pinMap = document.querySelector('.tokyo__pin-map');
-pinMap.appendChild(pinsList);
-var lodgeTemplate = document.querySelector('#lodge-template').content;
-var offerDialog = document.querySelector('#offer-dialog');
-var dialogPanel = offerDialog.querySelector('.dialog__panel');
-
 /**
  * Создаёт DocumentFragment спанов с особенностями
  * @param {Array.<string>} features
  * @return {DocumentFragment}
  */
 var getFeaturesFragment = function (features) {
-  var fragment = document.createDocumentFragment();
-  features.forEach(function (elem) {
+  return getFragment(features, function (feature) {
     var span = document.createElement('span');
     span.classList.add('feature__image');
-    span.classList.add('feature__image--' + elem);
-    fragment.appendChild(span);
+    span.classList.add('feature__image--' + feature);
+    return span;
   });
-  return fragment;
 };
 
 /**
@@ -228,12 +224,20 @@ var renderAd = function (ad) {
   adElement.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + ad.offer.checkin + ', выезд до ' + ad.offer.checkout;
   adElement.querySelector('.lodge__features').appendChild(getFeaturesFragment(ad.offer.features));
   adElement.querySelector('.lodge__description').textContent = ad.offer.description;
-  offerDialog.querySelector('.dialog__title').children[0].src = ad.author.avatar;
+  offerDialog.querySelector('.dialog__title img:first-child').src = ad.author.avatar;
 
   return adElement;
 };
 
-var adsList = getFragment(ads.slice(0, 1), renderAd);
+var imageAddresses = getImageAddress();
+var titles = getTitles();
+var ads = getRandomArray(NUMBER_OF_ADS, createAd, false);
+var pinsList = getFragment(ads, createPin);
+var pinMap = document.querySelector('.tokyo__pin-map');
+pinMap.appendChild(pinsList);
+var lodgeTemplate = document.querySelector('#lodge-template').content;
+var offerDialog = document.querySelector('#offer-dialog');
+var dialogPanel = offerDialog.querySelector('.dialog__panel');
 
 dialogPanel.innerHTML = '';
-dialogPanel.appendChild(adsList);
+dialogPanel.appendChild(renderAd(ads[0]));
